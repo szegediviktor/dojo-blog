85d4c8c4e2338bbb9c843ca7336643009f6a3fda
szegediviktor <szeg.viktor@gmail.com>
2022-02-11 10:22:13 +0100

    delete all the notes

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then((res) => {
-                // error-handling a fetchen belül. A response mindig egy object. Ezen belül van egy kulcs - ok. Ezt vizsgáljuk. Ha minden stabil akkor 'ok': true. Azonban ha !res.ok igaz, tehát false akkor Error-t dobunk. Például, ha nem jó az endpoint, nem tudja fetchelni az adott helyről az adatokat, de response jön, csak nem jó.
                 if (!res.ok) {
                     throw Error("Could not fetch the data for that resource");
                 }
@@ -22,28 +21,20 @@ const Home = () => {
                 setIsLoading(false);
                 setError(null);
             })
-            // Error-handling. .catch-elkapjuk az error és az erros stateba frissítjuk/beírjuk az error objektum message kulcsának értékét. Akkor akar itt fenn, ha előtte .then(res) és a .then(data) is failed. Mondjuk nem fut a server, nincs internet stb.
+
             .catch((err) => {
                 setIsLoading(false);
                 setError(err.message);
             });
-    }, []); // Ha üres tömb a dependency akkor csak egyszer fut le a useEffect, mégpedig az első renderelés után. Amit beírunk ide változót, azt fogja figyelni (watch) és csak annak változásakor fut le a useEffect tartalma.
+    }, []);
 
     return (
         <div className="home">
-            {/* Conditional rendering --> Ha a bal oldal true akkor végrehajtja a jobb oldalt */}
-
             {error && <div>{error}</div>}
 
             {isLoading && <div>Loading...</div>}
 
-            {blogs && (
-                <BlogList
-                    blogs={blogs}
-                    title="All blogs"
-                    // handleDelete={handleDelete}
-                />
-            )}
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
         </div>
     );
 };
