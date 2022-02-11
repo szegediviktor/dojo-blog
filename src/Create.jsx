344bc42cb7599a61a2f47c918ae49f44653010bf
szegediviktor <szeg.viktor@gmail.com>
2022-02-11 13:26:28 +0100

    add the create new blog feature

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Sütike");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {
+            title,
+            body,
+            author,
+        };
+        setIsLoading(true);
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(blog),
+        }).then(() => {
+            console.log("new blog added");
+            setIsLoading(false);
+        });
+    };
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Sütike">Sütike</option>
+                    <option value="Viktor">Viktor</option>
+                </select>
+                {!isLoading && <button>Add Blog</button>}
+                {isLoading && <button disabled> Adding Blog... </button>}
+            </form>
         </div>
     );
 };
