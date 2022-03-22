d464ee5d2cbf9be1b56da6a95e379868aa12f19d
szegediviktor <szeg.viktor@gmail.com>
2022-03-22 13:39:59 +0100

    fix the style

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title }) => {
     return (
-        <div className="blog-preview">
+        <div className="blog-preview-wrapper">
             <h1>{title}</h1>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
